172268983f800a7bd4a57b9b0e57f32bebe7e751
Vi <hrifo@mail.ru>
2022-03-03 01:42:55 +0300

    lab 2

---
+++ 02_Lab/Vector/vector.h
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "../Number/number.h"
+
+
+class Vector1
+{
+public:
+    Vector1() = default;
+    Vector1(Number x, Number y);
+
+    Number PolarRadius();
+
+    Number PolarAngle();
+
+    Vector1 VectorAddition(Vector1 vector2);
+
+    Number x_;
+    Number y_;
+
+private:
+
+};
