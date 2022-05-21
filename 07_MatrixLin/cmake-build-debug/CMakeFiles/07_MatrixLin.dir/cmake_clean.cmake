72296800fb5922ed4b4f218c2d6c20f3fa9d66e5
Victor <victortim2002@gmail.com>
2022-05-21 14:41:37 +0300

    6 lab

---
+++ 07_MatrixLin/cmake-build-debug/CMakeFiles/07_MatrixLin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "07_MatrixLin.exe"
+  "07_MatrixLin.exe.manifest"
+  "07_MatrixLin.pdb"
+  "CMakeFiles/07_MatrixLin.dir/main.cpp.obj"
+  "lib07_MatrixLin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/07_MatrixLin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
